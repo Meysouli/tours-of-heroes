e3553fadce0bb50f2a7e092f36acbf1a8334e441
Meysouli <oumeymasouli@gmail.com>
2023-01-12 11:59:39 +0100

    add: add tour of heroes project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientInMemoryWebApiModule, InMemoryDbService } from 'angular-in-memory-web-api';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {InMemoryDataService} from './services/in-memory-data.service';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {HeroSearchComponent} from './components/hero-search/hero-search.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {HeroDetailComponent} from './components/hero-detail/hero-detail.component';
+import {HeroService} from './services/hero.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+      delay: 300,
+      passThruUnknownUrl: true
+    })
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
